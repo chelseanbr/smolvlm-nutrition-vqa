50947f0454a6dac8ed6360496ef0fa8d0ea1216c
Chelsea R <chelseanbr@gmail.com>
2025-10-28 16:58:20 -0700

    save mem during eval

--- src/finetune.py
+++ src/finetune.py
@@ -235,6 +235,9 @@ def train(
         gradient_checkpointing=True,
         # per_device_eval_batch_size=4,
         per_device_eval_batch_size=1,
+        eval_accumulation_steps=1,
+        predict_with_generate=False,
+        gradient_checkpointing_kwargs={"use_reentrant": False}
     )
 
     # Define the partial function to inject the required data
